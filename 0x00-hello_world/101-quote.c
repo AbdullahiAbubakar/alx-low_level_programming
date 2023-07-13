515c71ca8e855d9d518c35a068fc4e073a2d4456
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-13 06:35:14 -0700

    preprocess only

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: returns zero
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 25-10-19,\n");
+	return (1);
+}
