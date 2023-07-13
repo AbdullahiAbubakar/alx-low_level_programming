2cf2b4287c111a7d730b74d947bb33bb8ff9174b
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-13 07:37:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 25-10-19,\n";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 25-10-19\n";
 
 	write(2, s, sizeof(s) - 1);
 	return (1);
