ffd5f4bd34c5c85db23747d6e5fd1e232a87a945
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-13 07:25:46 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 25-10-19,\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 25-10-19,\n");
 	return (1);
 }
