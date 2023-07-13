263b4bfed743c455dc8bc397693f6cad5b39da6b
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-13 07:36:04 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - this is the main function
  * Return: returns zero
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 25-10-19,\n");
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 25-10-19,\n";
+
+	write(2, s, sizeof(s) - 1);
 	return (1);
 }
