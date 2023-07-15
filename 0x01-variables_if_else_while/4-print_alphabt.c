79918214f17b82d0b4a0969a121a9c289142e68c
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-15 07:08:02 -0700

    print alphabet with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: returns zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
