52ced3d0b4934cdc4e9a1bc6163dfe399e6be8e9
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-15 07:32:51 -0700

    print base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: returns zero
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i <= 58; i++)
+	{
+		putchar(i);
+	}
+	for (j = 'a'; i <= 'f'; i++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
