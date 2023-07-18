22c42cd15dabe6d79e2fe5aaeb623d15194cbad7
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-18 07:42:41 -0700

    print putchar function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ * Return: returns zero
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
+
