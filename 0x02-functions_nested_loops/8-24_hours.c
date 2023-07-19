34d6fe2eed7236ac88f8df01357a8321c8fd52f9
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-19 09:51:11 -0700

    print twenty four hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,23 +5,15 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
 	for (i = 48; i <= 57; i++)
 	{
 		for (j = 48; j <= 57; j++)
 		{
-			for (k = 48; k <= 57; k++)
-			{
-				for (l = 48; l <= 57; l++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(k);
-					_putchar(l);
-				}
-			}
+			_putchar(i);
+			_putchar(j);
+			_putchar(':');
 		}
 	}
 }
