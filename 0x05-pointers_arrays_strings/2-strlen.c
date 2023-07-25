523edb916e62346f1b4d8979db8fd626c8c78023
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-25 03:56:55 -0700

    prints a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,11 +8,7 @@
 int _strlen(char *s)
 {
 	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		printf("%d", i);
-
-	}
+		
+	printf("%d", i);
 	return (0);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: a string
+ */
+void _puts(char *str)
+{
+	_puts(str);
+	_puts('\n');
+}
