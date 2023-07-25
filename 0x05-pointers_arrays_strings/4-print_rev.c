8f93e6cf0f33aeed6a0687622a9ea5c651e2e83e
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-07-25 09:32:55 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,8 @@
 void print_rev(char *s)
 {
 	int i;
-	
-	i = 0;
-	while (s[i] != '\0')
-	{
-		break; 
-	}
-	{
+	char c;
+
 		printf("%s", s);
 	}
 	i++;
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,13 +9,12 @@ void rev_string(char *s)
 {
 	int i, l;
 	char c;
+	
 	l = strlen(s);
-
-	for (i = 0; i <= l/2; i++)
+	for (i = 0; i < (l / 2); i++)
 	{
 		c = s[i];
 		s[i] = s[l - 1 - i];
 		s[l - 1 - i] = c;
 	}
-	printf("%s", s);
 }
