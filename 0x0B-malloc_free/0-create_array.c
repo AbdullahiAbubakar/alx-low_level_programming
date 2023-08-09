629a8adae6a4099d86a1d2cd9be6c71df9118d6f
Abdullahi_Abubakar <modibbogwandu@gmail.com>
2023-08-09 14:53:06 -0700

    return a pointer to array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ * @size: size
+ * @c: a character
+ * Return: returns an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	ar = malloc(size * sizeof(unsigned int));
+	if (size == 0 || ar == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
+	return (ar);
+}
+
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strdup - returns a pointer
+ * @str: a string
+ * Return: returns a pointer
+ */
+char *_strdup(char *str)
+{
+
